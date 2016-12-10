027a5afb08cd69dc9815c66de3e51515b223cf9a
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2016-12-10 14:21:17 -0500

    Added additional configuration options

--- oc_config.sh
+++ oc_config.sh
@@ -4,9 +4,14 @@
 # ----------------------------
 # Change OSHOME to your preferred location.
 # Change the NUMVOLUMES variable to specify how many PVs to set up
+# Change OSREGISTRY to use a different OpenShift registry (e.g. OpenShift Origin)
+# Change OSVERSION to use a different OpenShift version
 #
 # This script has been written for macOS but should work just as well on
 # any other Unix based environment
 
 OSHOME=$HOME/OSCluster
-NUM_VOLUMES=15
+
+OSREGISTRY=registry.access.redhat.com/openshift3/ose
+OSVERSION=v3.3.1.7
+NUM_VOLUMES=20
