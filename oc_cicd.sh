7d52c0094a3774679786eccc5ec899797bbae2b6
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2016-12-13 10:56:47 -0500

    Fixed project name

--- oc_cicd.sh
+++ oc_cicd.sh
@@ -16,7 +16,7 @@ spec:
 
 oc new-project cicd --display-name="CI/CD Shared Environment"
 createNexusPVC
-oc new-app sonatype/nexus3 --name=nexus3 -n cicd1
+oc new-app sonatype/nexus3 --name=nexus3 -n cicd
 oc expose svc/nexus3
 
 # First remove all volumes
@@ -27,4 +27,5 @@ oc volume dc nexus3 --add --name=nexus3-1 --type=PersistentVolumeClaim \
  --claim-name=nexus --mount-path=/nexus-data
 
 # Now add a Jenkins with persistent Storage
-oc new-app jenkins-persistent --param MEMORY_LIMIT=1Gi --param VOLUME_CAPACITY=5Gi --param JENKINS_PASSWORD=admin123 -n cicd
+oc new-app jenkins-persistent --param MEMORY_LIMIT=1Gi \
+ --param VOLUME_CAPACITY=5Gi --param JENKINS_PASSWORD=admin123 -n cicd
